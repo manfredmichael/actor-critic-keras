294617d3534bda65303d83876a7b928d52c5c2cf
manfred michael <koskosanrider@gmail.com>
2022-05-01 19:32:45 +0700

    Add main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import numpy as np
+from agent import Agent
+import itertools
+import gym
 
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Activation, Dense, Input
-from tensorflow.models import Model
-from tensorflow.keras.optimizers import Adam
+env = gym.make('LunarLander-v2')
 
-class Agent:
-    def __init__(self, alpha, beta, gamma=0.99, n_actions=4, fc1_size=1024, fc2_size=512,
-                 input_dims=8):
-        self.gamma = gamma
-        self.alpha = alpha
-        self.beta = beta
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_size
-        self.fc2_dims = fc2_size
+obs_shape = env.observation_space.sample().shape[0]
+act_shape = env.action_space.n
 
+agent = Agent(alpha=0.1e-5, beta=0.5e-3, input_dims=obs_shape, n_actions=act_shape)
+
+for episode in itertools.count():
+    observation = env.reset()
+    done = False
+    for timestep in itertools.count():
+        if episode % 50 == 0:
+            env.render()
+        action = agent.choose_action(observation)
+        observation_next, reward, done, info = env.step(action)
+        agent.learn(observation, action, reward, observation_next, done)
+
+        observation = observation_next
+
+        if done:
+            print(agent.report.report_episode())
+            break
